3ee96d62466b54c33736a0ede0d00707eab914bb
Ania <anna111239@gmail.com>
2025-08-01 01:18:32 +0200

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,31 @@
 import { defineConfig } from "vite";
 import { globSync } from "glob";
-import { resolve } from "path";
+import path, { resolve } from "path";
+
+// Отримуємо абсолютний шаблон
+const htmlFiles = globSync(
+  path.posix.join(__dirname.replace(/\\/g, "/"), "src/*.html")
+);
+
+const inputEntries = Object.fromEntries(
+  htmlFiles.map((file) => {
+    const name = path
+      .relative(resolve(__dirname, "src"), file)
+      .replace(/\\/g, "/");
+    return [name, file];
+  })
+);
+
+console.log("HTML files found:", htmlFiles);
+console.log("Rollup input:", inputEntries);
 
 export default defineConfig({
   root: "src",
   build: {
     rollupOptions: {
-      input: globSync(resolve(__dirname, "src", "*.html")),
+      input: inputEntries,
     },
     outDir: "../dist",
+    emptyOutDir: true,
   },
 });
